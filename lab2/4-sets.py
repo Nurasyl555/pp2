1247768bffaefcd6799b6f4527b0fa66b2146045
Iam <kalmahanbet.nur06@gmail.com>
2024-01-30 21:11:45 +0600

    lll

---
+++ lab2/4-sets.py
@@ -0,0 +1,17 @@
+# 1
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+# 2
+fruits = {"apple", "banana", "cherry"}
+fruit.add("orange")
+# 3
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruit.update(more_fruits)
+# 4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+# 5
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
